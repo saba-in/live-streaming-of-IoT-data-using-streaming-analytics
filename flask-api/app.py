170b8f3da999d814473bd74d889dbcf868dc4920
saba-in <64768584+saba-in@users.noreply.github.com>
2020-06-05 14:39:20 +0500

    Update app.py

--- flask-api/app.py
+++ flask-api/app.py
@@ -11,7 +11,7 @@ CORS(app)
 
 def realtime_simulation():
     df = pd.read_excel('final_streaming_sample.xlsx','final_streaming_sample')
-    df.pop('Vehicle Present')
+    #df.pop('Vehicle Present')
     df.columns = ["Area", "O_BetweenStreet1", "O_BetweenStreet2", "DewPointC", "DurationSeconds", "FeelsLikeC", "HeatIndexC", "Hourly_Counts", "Sensor_Id", "Sensor_Name", "Side_Of_Street", "StreetId", "StreetName", "Time", "WindGustKmph", "cloudcover", "humidity", "moon_illumination", "pressure", "tempC", "winddirDegree", "windspeedKmph"]
     df = df.sample()
     out = df.to_json(orient='records')[1:-1].replace('},{', '} {')
